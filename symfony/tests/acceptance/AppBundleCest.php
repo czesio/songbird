5dfc55b3436df83a65a94c5953b95fc3e4e6d214
Davi <dawid@acosiedziwisz.pl>
2017-05-05 16:03:27 +0200

    added codeception and created basic test

---
+++ symfony/tests/acceptance/AppBundleCest.php
@@ -0,0 +1,26 @@
+<?php
+
+
+class AppBundleCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
+
+    # replaced tryToTest function with InstallationTest function
+    public function InstallationTest(AcceptanceTester $I)
+    {
+         $I->wantTo('Check if Symfony is installed successfully.');
+         $I->amOnPage('/');
+         $I->see('Welcome to');
+    }
+}
