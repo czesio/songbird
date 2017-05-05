f1cef0f748fb0464457ec69d000af154606c2a87
Davi <dawid@acosiedziwisz.pl>
2017-05-05 16:17:10 +0200

    added codeception more test

--- symfony/tests/acceptance/AppBundleCest.php
+++ symfony/tests/acceptance/AppBundleCest.php
@@ -17,10 +17,26 @@ class AppBundleCest
     }
 
     # replaced tryToTest function with InstallationTest function
+    /*
     public function InstallationTest(AcceptanceTester $I)
     {
          $I->wantTo('Check if Symfony is installed successfully.');
          $I->amOnPage('/');
          $I->see('Welcome to');
     }
+    */
+
+    # replace installationTest with removalTest
+
+    /**
+      * check that homepage is not active
+      *
+      * @param AcceptanceTester $I
+      */
+     public function RemovalTest(AcceptanceTester $I)
+     {
+         $I->wantTo('Check if / is not active.');
+         $I->amOnPage('/');
+         $I->see('404 Not Found');
+     }
 }
